758ff9813037667fdc1c6c669bf81109da36bf1e
Raviast <raviastare6@gmail.com>
2024-12-08 11:24:59 +0530

    use state fix

--- src/components/LandingPage/ClientsSection.jsx
+++ src/components/LandingPage/ClientsSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import ClientCard from '../SharedComponents/ClientCard'; // Import the ClientCard component
 
 function HappyClients() {
